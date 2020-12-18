0f782dfe6aec83064f520b8edae87b2e5e6e79e2
NhonPham <nhonpham0211@gmail.com>
2020-12-18 17:48:10 +0700

    generate the initial project structure
    generate the initial project structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <div className="App">hello app</div>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
