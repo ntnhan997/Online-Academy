bbd879ae792d97349763643280f5888cdd154fa3
NhonPham <nhonpham0211@gmail.com>
2020-12-25 23:40:05 +0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
 
+import { Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
     </>
   );
 }
