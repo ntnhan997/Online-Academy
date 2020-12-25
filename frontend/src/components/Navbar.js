fcdef81cca4f4e0a3d83b527a413f46a5303aeb0
NhonPham <nhonpham0211@gmail.com>
2020-12-25 23:38:39 +0700

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,5 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
+import logo from "../images/logo.png";
+import { FaAlignRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
-export default function Navbar() {
-  return <div>Hello from navbar</div>;
+export default class Navbar extends Component {
+  state = {
+    isOpen: false,
+  };
+  handleToggle = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
+  render() {
+    return (
+      <nav className="navbar">
+        <div className="nav-center">
+          <div className="nav-header">
+            <Link to="/">
+              <img src={logo} alt="educate logo" />
+            </Link>
+            <button
+              type="button"
+              className="nav-btn"
+              onClick={this.handleToggle}
+            >
+              <FaAlignRight className="nav-icon" />
+            </button>
+          </div>
+          <ul
+            className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}
+          >
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/courses">Courses</Link>
+            </li>
+           <li>
+             <Link to="/">options</Link>
+           </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
