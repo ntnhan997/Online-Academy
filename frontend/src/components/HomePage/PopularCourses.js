7168ffd0e84b6ee87e824f7e4ccb4874faa232b9
NhonPham <nhonpham0211@gmail.com>
2021-01-06 16:43:50 +0700

    Created PopularCourses.js & CSS, Updated HomePage.js

---
+++ frontend/src/components/HomePage/PopularCourses.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { PopularWrapper } from "./PopularStyle";
+import Card from ".././Card";
+import Card03 from ".././Card03";
+import arthur from "../../images/arthur.png";
+import batman from "../../images/batman.png";
+import blackpanter from "../../images/blackpanter.png";
+import kashima from "../../images/kashima.png";
+
+export default function PopularCourses() {
+  return (
+    <PopularWrapper>
+      <h1>Popular Courses</h1>
+      <div className="container">
+        <div className="row">
+          <Card
+            title="What is Lorem Ipsum?"
+            images="https://ttol.vietnamnetjsc.vn/images/2018/05/25/13/40/net-cuoi-be-gai-9-1527053440039156820618.jpg"
+            old_price="9,999"
+            newPrice="9999"
+            rupess="$"
+            alt="batman"
+            exp_date="25-10-2020"
+          />
+          <Card
+            title="What is Lorem Ipsum?"
+            images={arthur}
+            old_price="599"
+            newPrice="500"
+            rupess="&#x20B9;"
+            alt="blackpanter"
+            exp_date="25-10-2020"
+          />
+          <Card
+            title="What is Lorem Ipsum?"
+            images={blackpanter}
+            old_price="7999"
+            newPrice="7000"
+            rupess="$"
+            alt="arthur"
+            exp_date="25-10-2020"
+          />
+          <Card
+            title="What is Lorem Ipsum?"
+            images={kashima}
+            old_price="999"
+            newPrice="500"
+            rupess="$"
+            alt="kashima"
+            exp_date="25-10-2020"
+          />
+        </div>
+      </div>
+    </PopularWrapper>
+  );
+}
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -3,12 +3,15 @@ import Hero from "../components/Hero";
 import ListLatestCourse from "../components/HomePage/ListLatestCourse";
 import MostRegisteredCategory from "../components/HomePage/MostRegisteredCategory";
 import MostViewedCourses from "../components/HomePage/MostViewedCourses";
+import PopularCourses from "../components/HomePage/PopularCourses";
+
 
 
 export default function HomePage() {
   return (
     <>
       <Hero></Hero>
+      <PopularCourses/>
       <ListLatestCourse />
       <MostViewedCourses />
       <MostRegisteredCategory />
