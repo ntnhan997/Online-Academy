93a6108f9ab0f4d26cfa77426215f2df93dc4d3b
ntnhan997 <ntnhan997@gmail.com>
2021-01-06 14:16:30 +0700

    create list latest course

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../components/Hero";
+import ListLatestCourse from "../components/ListLatestCourse";
 
 export default function HomePage() {
   return (
     <>
       <Hero></Hero>
+      <ListLatestCourse />
     </>
   );
 }
