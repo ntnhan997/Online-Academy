bacecaea2fde4c65e01d4fc598dbb33f1ea52625
ntnhan997 <ntnhan997@gmail.com>
2021-02-26 03:41:43 +0700

    update topRegisterCourse from courseAction.js (redux)

--- frontend/src/actions/courseAction.js
+++ frontend/src/actions/courseAction.js
@@ -1,4 +1,4 @@
-import { COURSE_REQUEST_LIST, COURSE_SUCCESS_LIST, COURSE_FAIL_LIST } from '../constants/courseConstants';
+import { COURSE_REQUEST_LIST, COURSE_SUCCESS_LIST, COURSE_FAIL_LIST, TOPCOURSEREGISTERED_FAIL_LIST,TOPCOURSEREGISTERED_SUCCESS_LIST,TOPCOURSEREGISTERED_REQUEST_LIST } from '../constants/courseConstants';
 
 import axios from "axios";
 
@@ -12,4 +12,14 @@ const TopCourseViews = () => async (dispatch) => {
     }
 }
 
-export {TopCourseViews}
+const TopCourseRegistered = () => async (dispatch) => {
+    try {
+        dispatch({type : TOPCOURSEREGISTERED_REQUEST_LIST});
+        const data = await axios.get("/api/course/mostregisteredcourses");
+        dispatch({type: TOPCOURSEREGISTERED_SUCCESS_LIST, payload: data.data});
+    } catch (error) {
+        dispatch({type: TOPCOURSEREGISTERED_FAIL_LIST, payload: error});  
+    }
+}
+
+export {TopCourseViews, TopCourseRegistered}
