6f94125721dd3392f6c4aa1d6659debe72b88bc4
ntnhan997 <ntnhan997@gmail.com>
2021-02-26 01:47:29 +0700

    update MostViewedCourse

--- frontend/src/components/HomePage/MostViewedCourses.js
+++ frontend/src/components/HomePage/MostViewedCourses.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import OwlCarousel from "react-owl-carousel";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
@@ -7,7 +7,26 @@ import "./style.css";
 import { PopularWrapper } from "./PopularStyle";
 import Card from '../Card';
 
+
+import { useDispatch, useSelector } from "react-redux";
+import { TopCourseViews } from "../../actions/courseAction.js";
+
 const MostViewedCourses = () => {
+
+  const List = useSelector(state => state.listView);
+  const {listViews} = List;
+  const dispatch = useDispatch();
+  if(listViews[0]){
+    console.log(listViews[0].CategoryName);
+  }else{
+    console.log("k co");
+  }
+
+  useEffect(() => {
+    dispatch(TopCourseViews());
+    return () =>{
+    }
+},[dispatch]);
 
   const [dataCard] = useState([
     {
@@ -94,21 +113,29 @@ const MostViewedCourses = () => {
   return (
     <div className="ListLatestCourse">
       <h3>Most Viewed Courses</h3>
-      <OwlCarousel className="owl-theme" loop margin={10} lazyLoad items={4} dots={false} nav navText={['<-Prev','Next->']}>
+      {
+        listViews[0] ? 
+        <OwlCarousel className="owl-theme" loop margin={10} lazyLoad items={4} dots={false} nav navText={['<-Prev','Next->']}>
         {
-          dataCard.map(item => {
+          listViews.map(item => {
             return <PopularWrapper><Card key = {item.courseId} className="cardCourse"
-                id = {item.courseId}
-                title = {item.title}
-                category = {item.category}
-                price = {item.price}
-                bgPhoto = {item.bgPhoto}
-                totalReviews={item.totalReviews}
-                ratingAverage = {item.ratingAverage}
+                id = {item.CourseId}
+                title = {item.CourseName}
+                category = {item.CategoryName}
+                price = {item.CoursePrice}
+                bgPhoto = {item.CourseImage}
+                totalReviews={item.CourseReviews}
+                ratingAverage = {item.CourseRatings}
+                descriptions = {item.CourseDescriptions}
+                TeacherName = {item.TeacherName}
+                Avatar = {item.Avatar}
             /></PopularWrapper>
           })
         }
       </OwlCarousel>
+      : "Loading..."
+
+      }
     </div>
   );
 };
