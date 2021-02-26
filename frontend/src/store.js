92ab735b4cd9bff9b251d36d0f820806a82810c7
ntnhan997 <ntnhan997@gmail.com>
2021-02-26 16:35:09 +0700

    update redux and delete comment//

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,17 @@
 import thunk from "redux-thunk";
 import {combineReducers , createStore, applyMiddleware, compose} from 'redux';
 
-import {TopNumberViewsReducer, TopCourseRegisteredReducer} from "./reducers/courseReducer";
+import {TopNumberViewsReducer,
+    TopCourseRegisteredReducer,
+    TopCourseNewReducer
+} from "./reducers/courseReducer";
 
 const initialState = {};
 
 const reducer = combineReducers({
     listView: TopNumberViewsReducer,
-    topCourseRegisterList: TopCourseRegisteredReducer
+    topCourseRegisterList: TopCourseRegisteredReducer,
+    topCourseNewList :TopCourseNewReducer
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
