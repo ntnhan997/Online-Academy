c90cffd3de45a0fc494e32c9bfca5acde491af1b
Anhnguyet1803 <nguyetpham180397@gmail.com>
2021-03-23 19:22:47 +0700

    add function query delete favoritecouse

--- backend/models/favoritecourse.model.js
+++ backend/models/favoritecourse.model.js
@@ -3,6 +3,9 @@ const db = require("../utils/db");
 module.exports = {
     all(id){
         return db.select("*").from("favoritecourse").innerJoin("course", "favoritecourse.CourseID", "course.CourseID").innerJoin("category", "category.CategoryID", "course.CategoryID").innerJoin("teacher", "course.CourseID", "teacher.CourseID").where("course.CourseStatus","=",1).andWhere("favoritecourse.AccountID","=", id);
+    },
+    delete(idCourse){
+        return db("favoritecourse").where("CourseID", idCourse).del();
     }
 
 
