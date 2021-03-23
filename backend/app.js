9085acd88bcf6ba887d394972800b3ffe13af9c1
NhonPham <nhonpham0211@gmail.com>
2021-03-23 11:50:25 +0700

    added userRoute

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,23 @@
-const express  = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 const app = express();
+
+const userRoute = require("./routes/user.route");
 
 const PORT = process.env.PORT || 5000;
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
-app.get("/", (req,res) => {
-    res.send({
-        message: "hello"
-    })
-})
-
+app.get("/", (req, res) => {
+  res.send({
+    message: "hello",
+  });
+});
 
 app.use("/api/auth", require("./routes/auth.route"));
-
+app.use("/api/user", userRoute);
 
 app.listen(PORT, () => {
-    console.log(`api running at http://localhost:${PORT}`);
-})
+  console.log(`api running at http://localhost:${PORT}`);
+});
