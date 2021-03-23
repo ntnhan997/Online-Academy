b8042bdd11dd0395ea82161af6293d48d918a915
NhonPham <nhonpham0211@gmail.com>
2021-03-23 12:21:55 +0700

    added top10lastupdate() model

--- backend/models/course.model.js
+++ backend/models/course.model.js
@@ -27,4 +27,30 @@ module.exports = {
       .orderBy("NumberOfViews", "desc")
       .limit(10);
   },
+  top10lastupdate() {
+    return db
+      .select(
+        "course.CourseID",
+        "course.CourseImage",
+        "course.CourseName",
+        "course.CourseSummary",
+        "course.CourseDescriptions",
+        "course.NumberOfViews",
+        "course.CourseRatings",
+        "course.CourseReviews",
+        "course.NumberOfRegistered",
+        "course.CoursePrice",
+        "category.CategoryID",
+        "course.CourseStatus",
+        "category.CategoryName",
+        "account.AccountID",
+        "account.FullName"
+      )
+      .from("course")
+      .innerJoin("category", "category.CategoryID", "course.CategoryID")
+      .innerJoin("teacher", "course.CourseID", "teacher.CourseID")
+      .innerJoin("account", "account.AccountID", "teacher.AccountID")
+      .orderBy("LastUpdate", "desc")
+      .limit(10);
+  },
 };
