955657d3b1aba4020fbd00876a627d0172a84137
NhonPham <nhonpham0211@gmail.com>
2021-03-23 12:59:43 +0700

    added coursesuggestion model

--- backend/models/course.model.js
+++ backend/models/course.model.js
@@ -93,4 +93,31 @@ module.exports = {
     );
     return list[0];
   },
+  coursesuggestion(CategoryID) {
+    return db
+      .select(
+        "course.CourseID",
+        "course.CourseImage",
+        "course.CourseName",
+        "course.CourseSummary",
+        "course.CourseDescriptions",
+        "course.NumberOfViews",
+        "course.CourseRatings",
+        "course.CourseReviews",
+        "course.NumberOfRegistered",
+        "course.CoursePrice",
+        "category.CategoryID",
+        "course.CourseStatus",
+        "category.CategoryName",
+        "account.AccountID",
+        "account.FullName"
+      )
+      .from("course")
+      .innerJoin("category", "category.CategoryID", "course.CategoryID")
+      .innerJoin("teacher", "course.CourseID", "teacher.CourseID")
+      .innerJoin("account", "account.AccountID", "teacher.AccountID")
+      .where("course.CategoryID", CategoryID)
+      .orderBy("NumberOfRegistered", "desc")
+      .limit(5);
+  },
 };
