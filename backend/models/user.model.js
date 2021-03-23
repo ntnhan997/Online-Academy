58c8519f564bbef20df6e16eee3789b96d78ecff
NhonPham <nhonpham0211@gmail.com>
2021-03-23 11:52:07 +0700

    added register models (add function)

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,15 +1,22 @@
 const db = require("../utils/db");
 
 module.exports = {
-    async singleByUserName(username){
-        const list = await db.select("*").from("account").where("UserName","=",username);
-        if(list.length === 0){
-            return null;
-        }
-
-        return list;
-    },
-    updateRefreshToken(id, refreshToken){
-        return db("account").where("AccountID", id).update("rfToken", refreshToken);
+  async singleByUserName(username) {
+    const list = await db
+      .select("*")
+      .from("account")
+      .where("UserName", "=", username);
+    if (list.length === 0) {
+      return null;
     }
-}
+
+    return list;
+  },
+  updateRefreshToken(id, refreshToken) {
+    return db("account").where("AccountID", id).update("rfToken", refreshToken);
+  },
+  async add(user) {
+    const id = await db("account").insert(user);
+    return id[0];
+  },
+};
