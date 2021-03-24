3129f389e0716c04cdaab3dff23557a1dafc57c8
Anhnguyet1803 <nguyetpham180397@gmail.com>
2021-03-24 20:52:03 +0700

    add function add rating

---
+++ backend/models/rating.model.js
@@ -0,0 +1,12 @@
+const db = require("../utils/db");
+
+module.exports = {
+    async add(rating){
+        const avg = await db.select("*").from("course").where("CourseID", rating.CourseID);// 5 5
+        const mul = avg[0].CourseReviews * avg[0].CourseRatings; // 25
+        const total = ((mul + rating.Score) / (avg[0].CourseReviews + 1)).toFixed(2); //  20/5 4
+        await db("course").where("CourseID", rating.CourseID).update("CourseRatings", total);
+        await db("course").where("CourseID", rating.CourseID).increment("CourseReviews");
+        return await db("rating").insert(rating);
+    }
+}
