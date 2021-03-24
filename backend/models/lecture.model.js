d929b02b341b236cc2daccddf7a71e145f1c4e34
NhonPham <nhonpham0211@gmail.com>
2021-03-24 19:52:39 +0700

    Added allByCourseId route

--- backend/models/lecture.model.js
+++ backend/models/lecture.model.js
@@ -1,6 +1,9 @@
 const db = require("../utils/db");
 
 module.exports = {
+    allByCourseId(idCourse, idUser){
+        return db.select("*").from("lecture").innerJoin("subscribedcourse", "subscribedcourse.CourseID", "lecture.CourseID").where("lecture.CourseID", idCourse).andWhere("lecture.LectureDeleted", 0).andWhere("subscribedcourse.AccountID", idUser);
+    },
     addCourseByTeacher(Course){
         return db("course").insert(Course);
     },
