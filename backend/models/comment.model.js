5642f2f85fb0b81a89b984b7a61856a3bdb40027
Anhnguyet1803 <nguyetpham180397@gmail.com>
2021-03-24 20:45:48 +0700

    added function get comment by a user

---
+++ backend/models/comment.model.js
@@ -0,0 +1,10 @@
+const db = require("../utils/db");
+
+module.exports = {
+    singleByCoureId(courseId, accountId){
+       // return db.select("FullName").from("comment").innerJoin("account", "account.AccountID", "comment.AccountID").where("CategoryDeleted", 0);
+        return db.raw(`SELECT account.FullName, comment.Comment, comment.DateOfComment FROM comment, account  WHERE comment.AccountID = account.AccountID AND comment.CourseID = ${courseId}`)
+    }
+
+
+}
