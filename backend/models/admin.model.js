8491010a031c217fad26cbc5b4aa2e7272dce7b4
Anhnguyet1803 <nguyetpham180397@gmail.com>
2021-03-24 20:29:46 +0700

    added function ListedTeacher adminModel

--- backend/models/admin.model.js
+++ backend/models/admin.model.js
@@ -4,5 +4,8 @@ module.exports = {
     async addUser(user){
         const id = await db("account").insert(user);
         return id[0];
-    }
+    },
+    listedTeacher(){
+        return db.raw("SELECT account.AccountID, account.UserName, account.FullName, account.Email FROM account WHERE account.AccountTypeID = 2");
+    },
 }
