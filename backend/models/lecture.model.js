0e737de7aefa75cffa1a9e58f652dbe483a12048
NhonPham <nhonpham0211@gmail.com>
2021-03-24 19:55:21 +0700

    added allByCourseIdNoUser model

--- backend/models/lecture.model.js
+++ backend/models/lecture.model.js
@@ -1,13 +1,30 @@
 const db = require("../utils/db");
 
 module.exports = {
-    allByCourseId(idCourse, idUser){
-        return db.select("*").from("lecture").innerJoin("subscribedcourse", "subscribedcourse.CourseID", "lecture.CourseID").where("lecture.CourseID", idCourse).andWhere("lecture.LectureDeleted", 0).andWhere("subscribedcourse.AccountID", idUser);
-    },
-    addCourseByTeacher(Course){
-        return db("course").insert(Course);
-    },
-    addLectureByTeacher(Lecture){
-        return db("lecture").insert(Lecture);
-    }
-}
+  allByCourseId(idCourse, idUser) {
+    return db
+      .select("*")
+      .from("lecture")
+      .innerJoin(
+        "subscribedcourse",
+        "subscribedcourse.CourseID",
+        "lecture.CourseID"
+      )
+      .where("lecture.CourseID", idCourse)
+      .andWhere("lecture.LectureDeleted", 0)
+      .andWhere("subscribedcourse.AccountID", idUser);
+  },
+  allByCourseIdNoUser(idCourse) {
+    return db
+      .select("*")
+      .from("lecture")
+      .where("lecture.CourseID", idCourse)
+      .andWhere("lecture.LectureDeleted", 0);
+  },
+  addCourseByTeacher(Course) {
+    return db("course").insert(Course);
+  },
+  addLectureByTeacher(Lecture) {
+    return db("lecture").insert(Lecture);
+  },
+};
