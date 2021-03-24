fac65813f8b0a71ee6247a7f5184e28f826f387b
Anhnguyet1803 <nguyetpham180397@gmail.com>
2021-03-24 20:48:41 +0700

    added function add comment

--- backend/models/comment.model.js
+++ backend/models/comment.model.js
@@ -4,6 +4,9 @@ module.exports = {
     singleByCoureId(courseId, accountId){
        // return db.select("FullName").from("comment").innerJoin("account", "account.AccountID", "comment.AccountID").where("CategoryDeleted", 0);
         return db.raw(`SELECT account.FullName, comment.Comment, comment.DateOfComment FROM comment, account  WHERE comment.AccountID = account.AccountID AND comment.CourseID = ${courseId}`)
+    },
+    add(comment){
+        return db("comment").insert(comment);
     }
 
 
