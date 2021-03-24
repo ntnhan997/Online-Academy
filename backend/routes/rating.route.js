c85f1cde1720f42bffc6d83af702ba5dfe675180
Anhnguyet1803 <nguyetpham180397@gmail.com>
2021-03-24 20:56:21 +0700

    git add router get a rating

--- backend/routes/rating.route.js
+++ backend/routes/rating.route.js
@@ -5,6 +5,18 @@ const router = express.Router();
 const auth = require("../middlewares/auth.mdw");
 
 
+router.get("/:CourseID", auth, async (req,res) => {
+    const CourseID = req.params.CourseID;
+    const AccountID = req.body.userId;
+    const list = await ratingModel.single(CourseID,AccountID);
+    if(list === null){
+        return res.send(list);
+    }
+    
+    return res.send(list[0]);
+})
+
+
 router.post("/", auth, async (req,res) => {
     const rating = req.body;
     rating.AccountID = req.body.userId;
