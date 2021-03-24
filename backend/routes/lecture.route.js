0282cb485ae81206fdddf4b2b5a009cd4c5f5220
NhonPham <nhonpham0211@gmail.com>
2021-03-24 19:53:59 +0700

    Added allByCourseId route

--- backend/routes/lecture.route.js
+++ backend/routes/lecture.route.js
@@ -5,28 +5,33 @@ const router = express.Router();
 
 const auth = require("../middlewares/auth.mdw");
 
+const moment = require("moment");
 
-const moment = require('moment');
+router.get("/:idCourse", auth, async (req, res) => {
+  const idCourse = req.params.idCourse;
+  const idUser = req.body.userId;
+  const list = await lectureModel.allByCourseId(idCourse, idUser);
+  res.send(list);
+});
 
-router.post("/createbyteacher", auth,  async(req,res) => {
+router.post("/createbyteacher", auth, async (req, res) => {
+  const list = req.body;
 
-    const list = req.body;
-    
-    list.Course.NumberOfViews = 0;
-    list.Course.CourseRatings = 0;
-    list.Course.CourseReviews = 0;
-    list.Course.NumberOfRegistered = 0;
-    list.Course.LastUpdate = moment().format("YYYY-MM-DD HH:mm:ss");
-    const CourseID = await lectureModel.addCourseByTeacher(list.Course);
-    list.Lecture.map(item => item.CourseID = CourseID[0]);
-    const Lecture = await lectureModel.addLectureByTeacher(list.Lecture);
-    let teacher = {};
-    teacher.AccountID = list.userId;
-    teacher.CourseID = CourseID[0];
-    await teacherModel.add(teacher);
-    return res.send({
-        complete: true
-    });
-})
+  list.Course.NumberOfViews = 0;
+  list.Course.CourseRatings = 0;
+  list.Course.CourseReviews = 0;
+  list.Course.NumberOfRegistered = 0;
+  list.Course.LastUpdate = moment().format("YYYY-MM-DD HH:mm:ss");
+  const CourseID = await lectureModel.addCourseByTeacher(list.Course);
+  list.Lecture.map((item) => (item.CourseID = CourseID[0]));
+  const Lecture = await lectureModel.addLectureByTeacher(list.Lecture);
+  let teacher = {};
+  teacher.AccountID = list.userId;
+  teacher.CourseID = CourseID[0];
+  await teacherModel.add(teacher);
+  return res.send({
+    complete: true,
+  });
+});
 
 module.exports = router;
